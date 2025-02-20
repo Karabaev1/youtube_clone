9a8332105b82869afa63a391b393e4633b2f6de7
Saad <timurkarabaev09@gmail.com>
2025-02-20 23:34:39 +0500

    Create userauth and models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+VISIBILITY = (
+    ("public", "Public"),
+    ("private", "Private"),
+    ("unlisted", "Unlisted"),
+    ("members_only", "Members Only")
+
+)
+
+def user_directory_path(instance, filename):
+    return "user_{0}/{1}".format(instance.user.id, filename)
+
+
+class Video(models.Model):
+    video = models.FileField(upload_to=user_directory_path)
+    image = models.ImageField(upload_to=user_directory_path, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    tags = TaggableManager()
+    date = models.DateTimeField(auto_now_add=True)
+    visibility = models.CharField(choices=VISIBILITY, max_length=100, default="public")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Channel():
+    pass
